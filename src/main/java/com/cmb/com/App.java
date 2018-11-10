bac04b618dd80ebe81e7b989ba50c787a76eedb8
cxf <cxfeugene@163.com>
2018-11-11 01:02:11 +0800

    FIRST commit

--- src/main/java/com/cmb/com/App.java
+++ src/main/java/com/cmb/com/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! 1234567890" );
+        System.out.println( "Hello World! 12345678901" );
     }
 }
 
