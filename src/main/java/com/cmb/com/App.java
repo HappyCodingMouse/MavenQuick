7fb6c960d0a7e4d5f1d7e45dd9835b58526cca2b
cxf <cxfeugene@163.com>
2018-11-11 00:28:23 +0800

    FIRST commit

---
+++ src/main/java/com/cmb/com/App.java
@@ -0,0 +1,15 @@
+package com.cmb.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! 1234567890" );
+    }
+}
+
+
